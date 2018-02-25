1114cf32acb4e5caeca508b1d59a76296a5845f0
Ostik266 <33750170+Ostik266@users.noreply.github.com>
2018-02-25 18:49:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 class Sorter {
   constructor() {
     // your implementation
-     this.sorter = []
+     this.sorter = [];
+     this.comparator;
   }
 
   add(element) {
@@ -23,12 +24,34 @@ class Sorter {
     // your implementation
   }
 
-  sort(indices) {
-   
+  sort(indices){
+    let new_arr = []
+    let indice = indices.sort()
+    indice.forEach(index => new_arr.push(this.sorter[index]))
+
+    if (this.comparator)
+      new_arr.sort(this.comparator)
+    else {
+      new_arr.sort((a,b) => {
+        if(typeof a === 'number' && typeof b === 'number')
+          return a - b
+        if(typeof a === 'object' || typeof b === 'object')
+          return 0
+        if (a < b)
+          return -1
+        if ( a > b)
+          return 1
+        return 0
+      })
+    }
+    indice.forEach((index, i) => {
+      this.sorter[index] = new_arr[i]
+    })
   }
+   
 
   setComparator(compareFunction) {
-    this.sorter.sort(compareFunction)
+    this.comparator = compareFunction
   }
 }
 
